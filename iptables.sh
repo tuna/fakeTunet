b203e29bfebde31a64e035446693335babf74b9a
王邈 <shankerwangmiao@gmail.com>
2016-05-14 16:17:06 +0800

    Update iptables

--- iptables.sh
+++ iptables.sh
@@ -5,17 +5,22 @@ WANIF="eth0"
 iptables -F
 iptables -t mangle -F
 iptables -t nat -F
+iptables -X OFFLINE
+iptables -F OFFLINE
 
 iptables -t mangle -A PREROUTING -i $LANIF -m set ! --match-set logged src -j MARK --set-mark 1
 iptables -t mangle -A PREROUTING -i $LANIF -m set --match-set logged src -j MARK --set-mark 2
 
-iptables -t nat -A PREROUTING -d 101.5.40.0/21 -j ACCEPT
-iptables -t nat -A OUTPUT -d 101.5.40.0/21 -j ACCEPT
-iptables -t nat -A POSTROUTING -d 101.5.0.0/21 -j ACCEPT
+iptables -t nat -A POSTROUTING -o $WANIF -j MASQUERADE
+iptables -t nat -A PREROUTING -p tcp -m tcp --dport 80 -i $LANIF -m mark --mark 0x01 -j DNAT --to 127.0.0.1:80
+iptables -t nat -A PREROUTING -d 166.111.204.120 -i $LANIF -j DNAT --to 127.0.0.1
 
-iptables -t nat -A PREROUTING -p tcp -m tcp --dport 80 -m mark --mark 0x1 -j DNAT --to-destination 101.5.40.1:80
-iptables -t nat -A OUTPUT -p tcp -m tcp --dport 80 -m mark --mark 0x1 -j DNAT --to-destination 101.5.40.1:80
-iptables -t nat -A PREROUTING -d 166.111.204.120 -j DNAT --to-destination 101.5.40.1
-iptables -t nat -A OUTPUT -d 166.111.204.120 -j DNAT --to-destination 101.5.40.1
-iptables -t nat -A POSTROUTING -s 101.5.0.0/16 -m mark --mark 0x2 -j MASQUERADE
-iptables -A FORWARD -m mark --mark 0x1 -j DROP
+
+iptables -N OFFLINE
+iptables -A OFFLINE -p udp -m udp --dport 53 -j RETURN
+iptables -A OFFLINE -j DROP
+
+iptables -P FORWARD DROP
+iptables -A FORWARD -m mark --mark 0x1 -i $LANIF -j OFFLINE
+iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -i $WANIF -j ACCEPT
+iptables -A FORWARD -o $WANIF -j ACCEPT
